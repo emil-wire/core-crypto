9812417fdb946340e1e2e02b2ce6c410ba209db2
Emil Abramov <emil.abramov@wire.com>
2024-02-22 12:30:19 +0100

    second try

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,5 @@
 // This file is only necessary so cdxgen doesn't fail the build
+rootProject.name = "core-crypto"
 // Include the sub-project by its directory path
 include(":core-crypto-kotlin")
 // Specify the location of the sub-project
