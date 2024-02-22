0f8a942dacccc7086a25c8505b2664c78967a0a9
Emil Abramov <emil.abramov@wire.com>
2024-02-22 15:30:17 +0100

    e

--- settings.gradle.kts
+++
@@ -1,6 +0,0 @@
-// This file is only necessary so cdxgen doesn't fail the build
-rootProject.name = "core-crypto"
-// Include the sub-project by its directory path
-include(":core-crypto-kotlin")
-// Specify the location of the sub-project
-project(":core-crypto-kotlin").projectDir = file("crypto-ffi/bindings")
