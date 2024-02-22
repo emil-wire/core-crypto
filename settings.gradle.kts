dedc2da3e30f885ba05d69ec1e7dbdb30badd377
Emil Abramov <emil.abramov@wire.com>
2024-02-22 12:20:37 +0100

    technolinator fix

---
+++ settings.gradle.kts
@@ -0,0 +1,5 @@
+// This file is only necessary so cdxgen doesn't fail the build
+// Include the sub-project by its directory path
+include(":core-crypto-kotlin")
+// Specify the location of the sub-project
+project(":core-crypto-kotlin").projectDir = file("crypto-ffi/bindings")
